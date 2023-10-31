8dda90aaa1eddd3433ca63790ce0dda406aaab1f
c0b22113 <c0b2211320@edu.teu.ac.jp>
2023-10-31 17:10:48 +0900

    障害物自動生成

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import sys
 import pygame as pg
+import random
+import time
+
+class Obstacle:
+    def __init__(self, x, y, width, height):
+        '''
+        障害物を生成する
+        '''
+        self.rect = pg.Rect(x, y, width, height)
+        self.created_time = time.time()
+
+    def is_expired(self, duration):
+        '''
+        障害物が duration 秒経過したかどうかを返す
+        '''
+        return time.time() - self.created_time > duration
 
 def main():
     pg.display.set_caption("弾幕ゲー")
@@ -12,6 +28,9 @@ def main():
     tmr = 0
     x = 300  # タンクの初期 x 座標
     y = 500  # タンクの初期 y 座標
+
+    obstacles = [] # 障害物のリスト
+    next_obstacle_time = time.time() + random.randint(1, 3) # 障害物を生成する時刻
 
     while True:
         for event in pg.event.get():
@@ -30,6 +49,17 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(tank_img, [x, y])
+
+        # 障害物を生成
+        if time.time() > next_obstacle_time and len(obstacles) < 5:
+            obstacles.append(Obstacle(random.randint(0, 600), random.randint(200, 400), 100, 20))
+            next_obstacle_time = time.time() + random.randint(1, 3) # 次の障害物を生成する時刻を更新
+
+        # 障害物を描画
+        for obstacle in obstacles[:]:
+            pg.draw.rect(screen, (126, 126, 126), obstacle.rect)
+            if obstacle.is_expired(15):
+                obstacles.remove(obstacle)
 
         pg.display.update()
         tmr += 1
